d84e241ef753b2c205e2d098753a3b5910166d5b
Daria Semenova <dasha.ign,semenova@gmail.com>
2022-04-07 22:32:17 +0300

    Add project files.

---
+++ PasswordGenerator/PasswordCreator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace PasswordGenerator
+{
+    class PasswordCreator
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string digits = "0123456789";
+        public const string symbols = "!@#$%^&*()-_=+<,>.";
+    //    string allSymbols = chars + chars.ToLower() + digits + symbols;
+
+        public static List<string> PasswordList(int count, int passwordLength)
+        {
+            var result = new List<string>();
+            do
+            {
+                string password = GeneratePassword(passwordLength);
+                if (!result.Contains(password))
+                    result.Add(password);
+            }
+            while (result.Count < count);
+            return result;
+        }
+
+        public static string GeneratePassword(int length = 13, List<string> existingPasswords = null)
+        {
+            string allSymbols = chars + chars.ToLower() + digits + symbols;
+            //4 Capital letters, 4 small letters, 4 digits & 1 special symbol
+            char[] password = new char[length];
+            var random = new Random();
+            var randomNumbers = Enumerable.Range(0, length).OrderBy(x => random.Next()).Take(length).ToList();
+            // we need to ensure first password symbol is going to be character
+            if (randomNumbers.ToArray()[0] == 12) 
+                randomNumbers = randomNumbers.ToArray().Reverse().ToList();
+            int specSymbolPosition = random.Next(1, length - 1);
+            foreach (var number in randomNumbers)
+            {
+                _ = randomNumbers.IndexOf(number) switch
+                {
+                    int i when (i >= 0 && i <= 7)  =>
+                        password[number] = randomNumbers.IndexOf(number) % 2 == 0 ?
+                                            chars[random.Next(0, 25)] :
+                                            chars.ToLower()[random.Next(0, chars.Length)],
+                    int i when i > 7 && i < 12 =>
+                        password[number] = digits[random.Next(0, digits.Length)],
+                    int i when i == 12 =>
+                    password[number] = symbols[random.Next(0, symbols.Length)],
+                    _ => password[number] =  allSymbols[random.Next(0, allSymbols.Length)]
+                };
+            }
+            return new string(password);
+        }
+
+        public static void WriteToExcel(List<string> passwordList)
+        {
+            using var workBook = new XLWorkbook();
+            {
+
+                var workSheet = workBook.Worksheets.Add("Пароли");
+                workSheet.Cell("A1").Value = "Список паролей";
+                int i = 2;
+                foreach (string password in passwordList)
+                {
+                    workSheet.Cell(i, 1).Value = password;
+                    i++;
+                }
+                workSheet.Column(1).AdjustToContents();
+                workBook.SaveAs("passwords.xlsx");
+            }
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.IO;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Список спецсимволов: {PasswordCreator.symbols}");
+            //string password =  PasswordCreator.GeneratePassword(6);
+            //Console.WriteLine(password.ToString());
+            Console.WriteLine("Введите количество");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину пароля");
+            int length = Convert.ToInt32(Console.ReadLine());
+            var passwordList = PasswordCreator.PasswordList(count, length);
+            //foreach (var password in passwordList)
+            //    Console.WriteLine(password);
+            Console.WriteLine("Записываю список сгенерированных паролей в файл passwords.xlsx,\n располагающийся в той же папке, что и сама программа генератора");
+            PasswordCreator.WriteToExcel(passwordList);
+            Console.WriteLine("Запись файла завершена");
+            if (File.Exists("passwords.xlsx"))
+            {
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = "passwords.xlsx";
+                process.Start();
+            }
+                
+        }
+
+        
+    }
+}
